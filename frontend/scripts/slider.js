87ff933e8b64aa9412f00649c6cc10f97f942c3b
leonardo-xompero <leonardo.xompero@studenti.unitn.it>
2021-12-07 16:15:28 +0100

    slider slider

--- frontend/scripts/slider.js
+++ frontend/scripts/slider.js
@@ -1,4 +1,4 @@
-var slider = document.getElementById("sliderInput");
+/* var slider = document.getElementById("sliderInput");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value; // Display the default slider value
 
@@ -6,3 +6,30 @@ output.innerHTML = slider.value; // Display the default slider value
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
+ */
+var valueBubble = '<output class="rangeslider__value-bubble" />';
+
+
+function updateValueBubble(pos, value, context) {
+  pos = pos || context.position;
+  value = value || context.value;
+  var $valueBubble = $('.rangeslider__value-bubble', context.$range);
+  var tempPosition = pos + context.grabPos;
+  var position = (tempPosition <= context.handleDimension) ? context.handleDimension : (tempPosition >= context.maxHandlePos) ? context.maxHandlePos : tempPosition;
+
+  if ($valueBubble.length) {
+    $valueBubble[0].style.left = Math.ceil(position) + 'px';
+    $valueBubble[0].innerHTML = value;
+  }
+}
+
+$('input[type="range"]').rangeslider({
+  polyfill: false,
+  onInit: function() {
+    this.$range.append($(valueBubble));
+    updateValueBubble(null, null, this);
+  },
+  onSlide: function(pos, value) {
+    updateValueBubble(pos, value, this);
+  }
+});
